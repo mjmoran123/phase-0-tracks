808261a6c308e937df9833bc08227dc1a84b0eb2
mjmoran123 <matthew.j.moran@gmail.com>
2016-05-25 17:37:13 -0500

    add name and noise questions

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,1 +1,8 @@
- 
+puts "What's the hamster's name?"
+name = gets.chomp
+puts "How noisy is your hamster? (1 is very quiet, 10 is REALLY LOUD)"
+noise = gets.chomp
+if noise.to_i < 11 && > 0
+	puts "Enter an integer from 1 to 10!"
+end
+noise=gets.chomp
