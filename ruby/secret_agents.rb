f8b4a02cf9bab430874a4b1da39dcc054b3a33fd
mjmoran123 <matthew.j.moran@gmail.com>
2016-05-26 17:59:04 -0500

    add code for encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,6 +5,19 @@
 # 	- to deal with the z to a case we will work with index mod 26 on this string "abcdefghijklmnopqrstuvwxyz"
 # - return the encrypted string
 
+def encrypt (str)
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	len = str.length
+	counter = 0
+	while counter < len
+		new_letter = alphabet[(alphabet.index(str[counter]) + 1) % 26]
+		encrypted_string = encrypted_string + new_letter
+		counter += 1
+	end
+	return encrypted_string
+end
+
+
 # decrypt method: takes in an encrypted string
 #  -iterate over each character in the string
 #  	- change each letter to the letter before in the alphabet
