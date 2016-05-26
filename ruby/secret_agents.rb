854ab39bd422290d68cb5366cd492b75a8c3671b
mjmoran123 <matthew.j.moran@gmail.com>
2016-05-26 17:52:34 -0500

    add pseudocode for encrypt and decrypt methods

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,1 +1,13 @@
- 
+#encrypt method: takes in a string
+# -iterate over each character in given string
+# -for each character we will advance one letter in the alphabet past it (b->c, h->i, z->a)
+# 	-to advance we can use the .next function which is fine except for going from z to a
+# 	- to deal with the z to a case we will work with index mod 26 on this string "abcdefghijklmnopqrstuvwxyz"
+# - return the encrypted string
+
+# decrypt method: takes in an encrypted string
+#  -iterate over each character in the string
+#  	- change each letter to the letter before in the alphabet
+#  	-this requires care with a->z as above in encrypt
+#  	-we will still use the "abcdefghijklmnopqrstuvwxyz" string % 26 to do this
+#  return the decrypted string
