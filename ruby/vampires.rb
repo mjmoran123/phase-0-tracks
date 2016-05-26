a38f02717d3fc5fb50669979084fd4f9c939d5d3
mjmoran123 <matthew.j.moran@gmail.com>
2016-05-26 12:26:19 -0500

    add is_vampire logic for age

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,17 @@
  puts "What is your name?"
  name = gets.chomp
 
- puts "How old are you?"
- age = gets.chomp
+ puts "How old are you? Enter an integer numeral to represent your age in years."
+ age = gets.chomp.to_i
 
- puts "What year were you born?"
- year = gets.chomp
+ puts "What year were you born? Enter in yyyy format."
+ birth_year = gets.chomp.to_i
+
+ if Time.new.year.to_i - birth_year = age || Time.new.year.to_i - birth_year - 1 = age #check to see if vampire is lying. allow for birthday past date in current year.
+ 	vampire = false
+ else
+ 	vampire = true
+ end 
 
  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
  garlic = gets.chomp
