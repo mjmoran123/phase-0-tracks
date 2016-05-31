48038fd21696781510cfe9794af902add28a213d
mjmoran123 <matthew.j.moran@gmail.com>
2016-05-31 18:54:45 -0500

    complete release 1

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -8,3 +8,20 @@ our_array.delete_at(2)
 p our_array #3
 our_array.insert(2, "graph paper") #4
 p our_array
+our_array.shift
+p our_array #5
+
+puts "Which item are you looking for in the array?"  #6
+item = gets.chomp.to_s
+if our_array.include?(item)
+	puts "#{item} was found"
+else
+	puts "So sorry, #{item} was not found."
+end
+
+new_array = ["an item", "another item", "chocolate", "chocolate with peanut butter"]
+super_array = our_array + new_array
+p super_array
+
+
+
