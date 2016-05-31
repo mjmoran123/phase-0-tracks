30bbbad9f1a00406d50cf14aff4f7f477a31d8c8
mjmoran123 <matthew.j.moran@gmail.com>
2016-05-31 18:39:08 -0500

    add 3-5

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,6 +1,10 @@
 our_array = []
 p our_array
 our_array << "pencil" << "ruler"
-our_array + ["paper"]
-our_array.concat(["compass"])
+our_array = our_array + ["paper"]
+our_array.concat(["compass", "staedler eraser"])
+p our_array #1 & 2
+our_array.delete_at(2)
+p our_array #3
+our_array.insert(2, "graph paper") #4
 p our_array
