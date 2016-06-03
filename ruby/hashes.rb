56efa5f7aa098ef26679ce0597eefc83adddb5ec
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-03 17:35:04 -0500

    add pseudocode

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,8 @@
- 
+ # PSEUDOCODE
+ # Initialize an array of appropriate questions to get at the information you would like from the user the hash.
+ # Initialize a hash with the appropriate symbols in place.
+ # Write a loop that prompts user and stores appropriate data.
+ # Print hash back for user.
+ # Prompt user for an update to a key.
+ # 	if change is desired, update key
+ # 	else print hash and exit.
