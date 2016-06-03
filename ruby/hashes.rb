1f2608a7cc7bdaaeabadb0a43440d7fd1ee77ef8
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-03 18:32:21 -0500

    add change_type method

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,28 +6,10 @@
  # Prompt user for an update to a key.
  # 	if change is desired, update key
  # 	else print hash and exit.
- 											#encode symbols with data type so correct conversion can be made S-string, I-int, B-boolean
-questions = {
-	name_S: "Enter Name:", 
-	age_I: "Enter Age:",
-	children_I: "Enter number of children:",
-	decortheme_S: "Enter your decor theme:",
-	solid_B: "Do you like solids? (y/n)",
-	stripe_B: "Do you like stripes? (y/n)",
-	pattern_B: "Do you like patterns? (y/n)",
-	budget_I: "How many dollars are you willing to spend?"
-	} 
 
- client = {}
-
- questions.each do |key,value| 
-
- 	puts value
- 	input = gets.chomp
-
-
- 	last_letter = key[-1]
- 	if last_letter == "I"
+ def change_type (key, input)           #method to choose type for data
+	last_letter = key[-1]
+	if last_letter == "I"
  		info = input.to_i
  	elsif last_letter == "B"
  		if input == "y"
@@ -39,9 +21,41 @@ questions = {
  		info = input.to_s
  	end
 
- 	client[key] = info
+end
+ 											#encode symbols with data type so correct conversion can be made S-string, I-int, B-boolean
+questions = {
+	name_S: "Enter Name:", 
+	age_I: "Enter Age:",
+	children_I: "Enter number of children:",
+	decor_theme_S: "Enter your decor theme:",
+	solid_B: "Do you like solids? (y/n)",
+	stripe_B: "Do you like stripes? (y/n)",
+	pattern_B: "Do you like patterns? (y/n)",
+	budget_I: "How many dollars are you willing to spend?"
+	} 
+
+ client = {}
+
+ questions.each do |key,value|    #iterate through each key/question pair
+
+ 	puts value
+ 	input = gets.chomp            #store user input in string input
+	info = change_type(key, input)              #change data according to last letter of key
+ 	client[key] = info          #populate client hash with data
  		
  end
 
- p client
+
+ p client                       #print client hash to error check
+
+ # puts "Which key would you like to update? If none, type none."
+ # update = gets.chomp
+ # if update != "none"
+ # 	sym = update.to_sym
+ # 	puts "What would you like to change it to?"
+ # 	update = gets.chomp
+
+
+ # 	client[update.to_sym]
+
 
