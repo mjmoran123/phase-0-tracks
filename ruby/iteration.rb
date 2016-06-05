22ee1b0930e9c05f246f23fb4d39e9a8b5e3edc3
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-05 14:52:21 -0500

    add release 0 method

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,1 +1,9 @@
- 
+def block_method
+	c = "hey"
+	d = "you"
+	puts "this is before we take the block parameter"
+	yield(c, d)
+	puts "this is after we take the block parameter"
+end
+
+block_method { |a, b| puts "I am the block parameter #{a}, #{b}." }
