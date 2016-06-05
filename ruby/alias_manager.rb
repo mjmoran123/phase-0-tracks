63a21282352360531c9c10054ff77426ce7191c0
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-05 17:01:49 -0500

    add pseudocode for spy algorithm

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,1 +1,9 @@
- 
+ # pseudocode for spy name algorithm
+
+ # Split the name string into an array of a first and last name
+ # Create a new array for the spy name that takes in the 1th element of the original array as the 0th and 0th as 1th
+ # Iterate through each name string and change letters accordingly
+ # 	downcase strings
+ # 	if on "aeiou" change mod 5
+ # 	if on "bcdfghjklmnpqrstvwxyz" and change mod 21
+ # Join the array of spy names into a new string
