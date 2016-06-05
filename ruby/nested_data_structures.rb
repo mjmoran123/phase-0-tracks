91ffb3cda84171cacef0dd1e933c2c2b5651f4d8
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-05 16:37:18 -0500

    update structure and print calls to nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -19,19 +19,19 @@ texas = {
 				"lr",
 				"dw",
 				"ry"
-		]
+		],
 		
 
-		indy_breakdown: [13,11,12,7,13,3,9,5,8,0]
+		indy_breakdown: [13,11,12,7,13,3,9,5,8,0],
 
 		scores: {
 			team: 30,
 			power: 43,
 			individual: 83,
 			relay: 24
-		}
+		},
 
-	}
+	},
 
 	a2: {
 		inits: ["ca",
@@ -49,19 +49,23 @@ texas = {
 				"cz",
 				"dz",
 				"wz"
-		]
+		],
 		
 
-		indy_breakdown: [11,9,10,5,11,1,5,3,6,0]
+		indy_breakdown: [11,9,10,5,11,1,5,3,6,0],
 
 		scores: {
 			team: 25,
 			power: 39,
 			individual: 63,
 			relay: 11
-		}
+		},
 
 	}
 
 
 }
+
+p texas[:a2][:inits][14]
+p texas[:a1][:indy_breakdown][7]
+p texas[:a1][:scores][:relay]
