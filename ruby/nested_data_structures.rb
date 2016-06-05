10e138a5430d08cfe104eb65e3351721e1860e68
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-05 14:42:56 -0500

    update nested structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,37 @@
- 
+#Uh, I'm sitting here grading at the ARML meet, so lets make a nested
+# data structure to represent some data about the texas team
+
+
+texas = {
+	a1: {
+		inits: ["aa",
+				"kc",
+				"kf",
+				"jh",
+				"li",
+				"ai",
+				"jl",
+				"dl",
+				"dl",
+				"hl",
+				"mm", 
+				"sm",
+				"lr",
+				"dw",
+				"ry"
+		]
+		
+
+		indy_breakdown: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+		scores: {
+			team: _,
+			power: _,
+			individual: _,
+			relay: _
+		}
+
+	}
+
+
+}
