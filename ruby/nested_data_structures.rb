1d0fdda19d9e35f19832cf7040ffc11747ef79ad
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-05 16:32:41 -0500

    complete structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -22,13 +22,43 @@ texas = {
 		]
 		
 
-		indy_breakdown: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		indy_breakdown: [13,11,12,7,13,3,9,5,8,0]
 
 		scores: {
-			team: _,
-			power: _,
-			individual: _,
-			relay: _
+			team: 30,
+			power: 43,
+			individual: 83,
+			relay: 24
+		}
+
+	}
+
+	a2: {
+		inits: ["ca",
+				"dc",
+				"ad",
+				"kf",
+				"vg",
+				"pl",
+				"mm",
+				"rm",
+				"jp",
+				"zr",
+				"tt", 
+				"cw",
+				"cz",
+				"dz",
+				"wz"
+		]
+		
+
+		indy_breakdown: [11,9,10,5,11,1,5,3,6,0]
+
+		scores: {
+			team: 25,
+			power: 39,
+			individual: 63,
+			relay: 11
 		}
 
 	}
