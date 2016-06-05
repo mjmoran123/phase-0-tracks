5b0f3de46f1c5a5322a25a6f38350dcaf420f909
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-05 16:22:46 -0500

    complete exercises

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -22,21 +22,22 @@ puts "the usual" << " suspects"
 puts "the usual" + " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts " suspects".insert(0,"the usual")
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".getbyte(0)
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
