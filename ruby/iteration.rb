3ee6e74461423dbc7bc646e08ed2810f71237fa0
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-05 15:13:14 -0500

    add release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+#Release 0
+
 def block_method
 	c = "hey"
 	d = "you"
@@ -7,3 +9,26 @@ def block_method
 end
 
 block_method { |a, b| puts "I am the block parameter #{a}, #{b}." }
+
+#Release 1
+
+movies = ["The Godfather", "Citizen Kane", "Birdman"]
+best_pictures = {1974 => "The Godfather", 1941 => "Citizen Kane", 2014 => "Birdman"}
+
+#iterate through array
+movies.each do |film|
+	puts film
+end
+
+#iterate through hash
+best_pictures.each do |year, winner|
+	puts "In #{year} the best picture goes to... #{winner}."
+end
+
+p movies
+
+movies.map! do |film|
+	film.upcase
+end
+
+p movies
