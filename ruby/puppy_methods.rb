018a76792c248173bf89cbe0c2f58a26f7f3c590
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-06 18:12:31 -0500

    add r0 methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
-end 
+  def speak(number_of_woofs)
+  	number_of_woofs.times { puts "Woof!" }
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_age)
+  	7 * human_age
+  end
+
+  def play_dead
+  	puts "..."
+  end
+
+end
+
+spike = Puppy.new
+spike.fetch("ball")
+spike.speak(3)
+spike.roll_over
+p spike.dog_years(10)
+spike.play_dead
 
