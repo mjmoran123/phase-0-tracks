8d54d9dc6333e89b3f35b9682b1f7fbe4656c11a
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-07 21:52:54 -0500

    add reflection

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -77,3 +77,26 @@ item_quantity(groceries, "ice cream", 1)
 
 #Print List
 print_list(groceries)
+###############################
+# REFLECTION
+# What did you learn about pseudocode from working on this challenge?
+# I learned that the more specific you pseudocode is, the easier it is to implement.
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# Arrays don't allow to handle quantites as easily. Hashes allow for this with no hassle.
+
+# What does a method return?
+# You can make a method return whatever you want. Methods return the last thing they do.
+
+# What kind of things can you pass into methods as arguments?
+# Anything you want, as long as you write the method to handle the input appropriately.
+
+# How can you pass information between methods?
+# Without writing classes, you can pass information between methods by using parameters.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# Some ideas about the scope of a method were definitely strengthened. I still have some
+# questions about how this would be different with classes and calling the methods with
+# a .method approach.
+
+
