7d264c89539edb21ee6ff0e69d69c3924cb9b2ef
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-07 21:47:42 -0500

    add remaining methods and driver code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -26,12 +26,54 @@ end
 # use .delete to delete key value pair
 # output: updated hash
 
+def del_item(list, item_to_del)
+  list.delete(item_to_del)
+end
+
 # Method to update the quantity of an item
 # input: string of item name, new quantity convert to int
 # update quantity by using list[item_name] = integer
 # output: updated hash
 
+def item_quantity(list, item_to_update, quantity)
+  list[item_to_update] = quantity 
+end
+
 # Method to print a list and make it look pretty
 # input: hash
 # use EACH to print item and quantity with extra text as necessary to make pretty
 # output: string output
+
+def print_list(list)
+  puts "Shopping List"
+  list.each { |item, quantity| puts "You need #{quantity} #{item}(s)." }
+end
+
+
+### TESTING CODE
+
+#Initial Grocery List
+groceries = make_list("lettuce yogurt chicken")
+  #Print List
+  p groceries
+
+#Adding items to grocery list
+add_item(groceries,"lemonade", 2)
+add_item(groceries,"tomatoes", 3)
+add_item(groceries,"onion", 1)
+add_item(groceries,"ice cream", 4)
+  #Print List
+  p groceries
+
+#Remove items from list
+del_item(groceries, "lemonade")
+  #Print List
+  p groceries
+
+#Update Quanity
+item_quantity(groceries, "ice cream", 1)
+  #Print List
+  p groceries
+
+#Print List
+print_list(groceries)
