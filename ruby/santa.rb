4e2294cb04317f17da8cfec3ae5a4e57a59655c6
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-10 10:04:54 -0500

    add age, gender, ethnicity instance variables and initialize lots of diverse santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,13 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = 0
 	end
+
+	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
 	def speak
 		puts "Ho, ho, ho! Haaappy holidays!"
@@ -13,6 +18,18 @@ class Santa
 	end
 end
 
-claus = Santa.new
-claus.speak
-claus.eat_milk_and_cookies("chocolate chip")
+# claus = Santa.new
+# claus.speak
+# claus.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+example_genders = ["female", "male", "fluid", "agender", "bigender", "N/A"]
+example_ethnicities = ["Albanian", "Basque", "Congolese"]
+
+example_genders.length.times do |i|
+	example_ethnicities.length.times do |j|
+		santas << Santa.new(example_genders[i], example_ethnicities[j])
+	end
+end
+
+p santas
