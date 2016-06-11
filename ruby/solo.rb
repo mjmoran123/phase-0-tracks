839173bf516094d68448d2d18db56996b26f6750
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-10 19:41:24 -0500

    add class design for minion class

---
+++ ruby/solo.rb
@@ -0,0 +1,6 @@
+# Minion class
+# read and write attributes: accessory
+# read attributes: number of eyes, hair type, body type
+# methods: greet (greets you with a friendly "Bello!")
+# 		 eat_bananas (takes in an integer and eats that number of bananas)
+# 		 works (works in lab, randomly, with 70% chance, the work will result in explosion)
