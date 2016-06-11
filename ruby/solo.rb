f60f50dbb05067da44360d629e4cb00b866e1fb5
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-10 23:47:20 -0500

    add user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -34,24 +34,68 @@ class Minion
 end
 
 ########## RELEASE 1 DRIVER CODE #############
-example_accessories = [
-							"banana",
-							"uke",
-							"Teem",
-							"freeze ray",
-							"papoy", 
-							"technical drawings",
-							"bomb",
-							"poochie",
-							"hypno hat",
-							"crown"
-						]
-dave = Minion.new(2,"middle_part","average_pill")
-dave.accessory = example_accessories.sample
-dave.greet
-dave.eat_bananas(3)
-5.times { dave.work }
-p dave.accessory
-p dave.eyes
-p dave.hair
-p dave.body
+# example_accessories = [
+# 							"banana",
+# 							"uke",
+# 							"Teem",
+# 							"freeze ray",
+# 							"papoy", 
+# 							"technical drawings",
+# 							"bomb",
+# 							"poochie",
+# 							"hypno hat",
+# 							"crown"
+# 						]
+# dave = Minion.new(2,"middle_part","average_pill")
+# dave.accessory = example_accessories.sample
+# dave.greet
+# dave.eat_bananas(3)
+# 5.times { dave.work }
+# p dave.accessory
+# p dave.eyes
+# p dave.hair
+# p dave.body
+
+
+def print_minions(minion_array)
+	minion_count = 1
+	minion_array.each do |minion|
+
+		puts "Minion  # #{minion_count}"
+		puts "Minion has #{minion.eyes} eyes, a #{minion.hair} hairstyle, a #{minion.body} body type, and is carrying a #{minion.accessory}."
+		minion_count += 1 	
+	end
+end
+
+def user_interface
+	los_minions = []
+	flag = true
+	while flag
+		puts "Make a minion. To begin, enter your minion's number of eyes. When done making minions, enter quit"
+		info = gets.chomp
+		if info == "quit"
+			flag = false
+			break
+		else
+			eyes = info.to_i
+		end
+		puts "Enter the minion's hair style."
+		hair = gets.chomp
+		puts "Enter your minion's body type."
+		body = gets.chomp
+		puts "Finally, give your minion an accessory. Might I recommend a banana..."
+		accessory = gets.chomp
+		a_minion = Minion.new(eyes,hair,body)
+		a_minion.accessory = accessory
+		los_minions << a_minion	
+	end
+	print_minions(los_minions)
+end
+
+user_interface
+
+
+
+
+
+
