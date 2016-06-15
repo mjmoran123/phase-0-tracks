eb52d43650a67932d078a3da48cead57a518219b
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-14 20:19:29 -0500

    add reverse method and driver code with conditional

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,22 @@
 // 	- use the length to iterate through the word in reverse order (using the index value of each character in the string character array)
 // 	- concat the new variable each iteration through the loop
 // 	- exit the loop and return the new string
+
+function reverse(str) {
+	var reversedString = "";
+	var len = str.length
+	for(var i = 1; i <= len; i++){ //start index at 1 to account for zero index of char array
+		reversedString += str[len - i];
+	}
+	return reversedString;
+}
+
+var answer = reverse("Fleetomeremoteelf"); //not a palindrome since the first F is capitalized :-)
+var lessSnarkyAnswer = reverse("Reallynotapalindrome");
+
+var waterWet = true;
+
+if(waterWet){
+	console.log(answer);
+	console.log(lessSnarkyAnswer);
+}
