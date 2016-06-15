f3d442b9ef23b265efd255d69ba8005ed56e5f36
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-14 20:04:55 -0500

    add pseudocode for reverse method

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// in order to return a reversed string of the string passed we will
+// 	- create a new variable to store the reversed string
+// 	- find the length of the given string
+// 	- use the length to iterate through the word in reverse order (using the index value of each character in the string character array)
+// 	- concat the new variable each iteration through the loop
+// 	- exit the loop and return the new string
