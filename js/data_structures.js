83a264c414696d71f7645edf5a543099b77a0291
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-17 17:17:37 -0500

    add horses object from colors and names arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,1 +1,15 @@
- 
+var colors = ["blue", "raspberry ombre", "green", "silver sparkles"];
+var names = ["Ed", "Sea", "Work", "Saw"];
+
+colors.push("red");
+names.push("Jerry");
+
+console.log(colors);
+console.log(names);
+
+var horses = {};
+for(var i = 0; i < names.length; i++){
+	horses[names[i]] = colors[i];
+}
+
+console.log(horses);
