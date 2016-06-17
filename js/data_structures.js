61cf6aa264c498e254a73b01f9c66b8f0c68ff22
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-17 17:33:21 -0500

    add Car constructor and driver code

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,28 @@ for(var i = 0; i < names.length; i++){
 }
 
 console.log(horses);
+
+function Car(make, model, color, year, runsWell) {
+	// In this context, 'this' refers to 
+	// the individual dog we're making
+	//console.log("Our new dog:", this);
+	
+	// Therefore, this.name is sort of the Ruby
+	// equivalent of @name
+	this.make = make;
+	this.model = model;
+	this.color = color;
+	this.year = year;
+	this.runsWell = runsWell;
+	
+	// As for behavior, a function is a perfectly 
+	// valid property value ... whoa!
+	this.honk = function() { console.log("Beep! Beep!"); };
+	
+}
+
+var toyota = new Car("Toyota", "Prius", "Red", 2020, false);
+var honda = new Car("Honda", "Civic", "Grey", 1997, true);
+toyota.honk();
+honda.honk();
+console.log(toyota.model);
