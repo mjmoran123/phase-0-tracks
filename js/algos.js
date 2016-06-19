4fe2384e0e74d47005db8e451e277f41a8affdd6
mjmoran123 <matthew.j.moran@gmail.com>
2016-06-19 09:14:51 -0500

    add pseudocode for max string in given array

--- js/algos.js
+++ js/algos.js
@@ -1,1 +1,6 @@
- 
+//PSEUDOCODE SOLUTION TO R0 -- LONGEST STRING IN ARRAY
+ // - initialize a variable (maxString) to a blank string that will hold the longest string
+ // - iterate (FOR loop based on index in array that terminates based on length of array) through the given array and compute the length of each string as you encounter it
+ // - compare the length of the current string to the length of the string in the maxString variable
+ // - at the end of the iteration, return the string in maxString.
+ // - note that if there are two or more words that are the max and have same number of characters, my code will return the first such word. 
