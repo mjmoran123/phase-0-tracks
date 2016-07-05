3e36409eb042327b379a4bbf5602da0b1765eaa9
mjmoran123 <matthew.j.moran@gmail.com>
2016-07-05 17:39:15 -0500

    add dims,pos and you win on click alert

--- web_dev/jquery/scripts.js
+++ web_dev/jquery/scripts.js
@@ -2,6 +2,10 @@ $(document).click(function(event){
 	makeStylizedRectangle();
 });
 
+$('div').click(function(event){
+	alert("YOU WIN!");
+});
+
 function randomIntFromInterval(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
@@ -11,6 +15,9 @@ function makeStylizedRectangle(){
 	var colors = [];
 	var r,g,b;
 
+	var pos = [];
+	var dims = [];
+
 	for (var i = 0; i < 3; i++) {
 		colors[i] = randomIntFromInterval(0,255);
 	}
@@ -19,8 +26,20 @@ function makeStylizedRectangle(){
 	g = colors[1].toString();
 	b = colors[2].toString();
 
+	for (var j = 0; j < 2; j++) {
+		pos[j] = randomIntFromInterval(0,500);
+	}
+
+	for (var k = 0; k < 2; k++) {
+		dims[k] = randomIntFromInterval(0,200);
+	}
+
 	$('div').css({
-		'background-color': 'rgb(' + r + ',' + g + ',' + b + ')'
+		'background-color': 'rgb(' + r + ',' + g + ',' + b + ')',
+		'top': pos[0].toString() + 'px',
+		'left': pos[1].toString() + 'px',
+		'height': dims[0].toString() + 'px',
+		'width': dims[1].toString() + 'px'
 	});
 }
 
