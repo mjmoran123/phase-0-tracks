6f70be8179e549d4db390c37c07f063520fcadea
mjmoran123 <matthew.j.moran@gmail.com>
2016-07-05 17:29:29 -0500

    add function to color rectangle randomly

--- web_dev/jquery/scripts.js
+++ web_dev/jquery/scripts.js
@@ -1,3 +1,28 @@
 $(document).click(function(event){
-	alert( "Thanks for visiting!");
+	makeStylizedRectangle();
 });
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function makeStylizedRectangle(){
+	var colors = [];
+	var r,g,b;
+
+	for (var i = 0; i < 3; i++) {
+		colors[i] = randomIntFromInterval(0,255);
+	}
+
+	r = colors[0].toString();
+	g = colors[1].toString();
+	b = colors[2].toString();
+
+	$('div').css({
+		'background-color': 'rgb(' + r + ',' + g + ',' + b + ')'
+	});
+}
+
+
+
